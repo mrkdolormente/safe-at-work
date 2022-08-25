2cf0d365aab00cc57d8ab349206d90f197364ef3
Mark Dolormente <markjosephandinodolormente@gmail.com>
2022-08-25 16:56:06 +0800

    Change app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'safe-at-work'`, () => {
+  it(`should have as title 'safe-anywhere'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('safe-at-work');
+    expect(app.title).toEqual('safe-anywhere');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('safe-at-work app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'safe-anywhere app is running!'
+    );
   });
 });
