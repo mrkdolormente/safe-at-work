beac133a545172669aecea17ebf1a3a8db324001
Mark Dolormente <markjosephandinodolormente@gmail.com>
2022-08-25 16:52:52 +0800

    Changed tailwind prefix

--- src/app/shared/container/container.component.ts
+++ src/app/shared/container/container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-container',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.scss'],
 })
 export class ContainerComponent implements OnInit {
+  @Input('customClass') customClass: string = 'sw-flex sw-flex-col sw-justify-center';
   constructor() {}
 
   ngOnInit(): void {}
